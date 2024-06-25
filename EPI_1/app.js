e4b931056d3851dea7b667c12e8902cd35e873e1
suletha <suletha.gopi@gmail.com>
2024-06-25 06:49:41 +0530

    sibiling_le

--- EPI_1/app.js
+++ EPI_1/app.js
@@ -1,12 +1,34 @@
 {/* <div id="parent">
-    <div id="child">
+    <div id="child1">
         <h1>Im h1 tag</h1>
+        <h2>Im sibiling of h1</h2>
+    </div>
+    <div id="child2">
+        <h1>Im h1 tag</h1>
+        <h2>Im sibiling of h1</h2>
     </div>
 </div> */}
 
-const parent = React.createElement("div",{id:"parent"},React.createElement("div",{id:"child"},React.createElement("h1",{},"Im h1 tag")))
+const parent = React.createElement("div", { id: "parent" },
+    [
+      React.createElement("div", { id: "child1" },
+        [
+          React.createElement("h1", {}, "I'm h1 tag"),
+          React.createElement("h2", {}, "I'm sibling of h1")
+        ]
+      ),
+      React.createElement("div", { id: "child2" },
+        [
+          React.createElement("h1", {}, "I'm h1 tag"),
+          React.createElement("h2", {}, "I'm sibling of h1")
+        ]
+      )
+    ]
+  );
+  
+  
 
-
+console.log(parent)
 //const heading = React.createElement("h1",{id:"heading"},"Hello World from React!")
 const root = ReactDOM.createRoot(document.getElementById("divEle"));
 root.render(parent)
